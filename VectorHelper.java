009953a397d4ef9117667b1f4ad6e5e428a0721d
hichemboukhemia <fh_boukhemia@esi.dz>
2017-10-23 02:35:13 +0100

    made changes in VectorHelper.java

--- VectorHelper.java
+++ VectorHelper.java
@@ -13,6 +13,33 @@ public  class VectorHelper {
     public VectorHelper() {
         // TODO Auto-generated constructor stub
     }
+
+    /**
+     * @param vector non sorted to be sorted
+     * @return a sorted vector
+     */
+    public static int [] tri (int vector[]){
+        /**
+         * i index of a vector
+         * j index of a vector
+         * permut is a variable
+         */
+        int i,j = 0 ;
+        int permut = 0 ;
+        for (i=0 ; i<vector.length ; i++ ){
+            for(j=i; j<vector.length ; j++ ){
+
+                if (vector[j]<vector[i]){
+                    permut=vector[i] ;
+                    vector[i]=vector[j];
+                    vector[j]=permut;
+                }
+
+
+            }
+        }
+        return ( vector ) ;
+    }
     /**
      *Addition of two vectors
      * @param vector1 
@@ -34,6 +61,30 @@ public  class VectorHelper {
         }
         return(vector3);
 
+    }
+    /**
+     *
+     * @param vector to inverse
+     * @return inverted vector
+     */
+    public static int [] InverserVector (int vector[]){
+        /**
+         * i index to browse a vector
+         * j index to browse a vector
+         * inverse variable
+         */
+        int i=0;
+        int j=vector.length;
+        int inverse;
+        for (i=0;i<(vector.length)/2;i++ )
+        {
+            inverse = vector[i];
+            vector[i]=vector[vector.length-i-1];
+            vector[vector.length-i-1]=inverse;
+
+        }
+
+        return(vector) ;
     }
 
     /**
@@ -64,6 +115,24 @@ public  class VectorHelper {
         return (vecteur);
 
     }
+    /**
+
+     * @param vector on which we apply a function
+     * @return a vector after a function is applied on its elements every element power 2
+     */
+    public static int [] Formule (int vector []) {
+        /**
+         * i index to browse a vector
+         */
+        int i = 0 ;
+        for(i=0 ; i< vector.length ; i++){
+            vector[i]= vector[i]*vector[i];
+        }
+
+        return(vector);
+    }
+
+
 
 
 
