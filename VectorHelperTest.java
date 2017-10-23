77cf34ef9ab8664d67e8330266591bcdfa26593f
hichemboukhemia <fh_boukhemia@esi.dz>
2017-10-23 02:44:32 +0100

    made changes in VectorHelperTest.java

--- VectorHelperTest.java
+++ VectorHelperTest.java
@@ -4,6 +4,15 @@ import static org.junit.Assert.*;
 
 
 public class VectorHelperTest {
+    @Test
+    public void tri() {
+
+         int vector1[]={1,9,6,2} ;
+         int vectorExpecTri []= {1,2,6,9};
+         int  TestTri [] = VectorHelper.tri(vector1);
+         assertArrayEquals(vectorExpecTri,TestTri);
+
+    }
 
     @Test
     public void somme() throws LengthException {
@@ -32,6 +41,14 @@ public class VectorHelperTest {
     }
 
     @Test
+    public void inverserVector()  {
+        int vector4[]={1,2,3,4,5} ;
+        int vectorExpecIve []= {5,4,3,2,1};
+        int  TestInver [] = VectorHelper.InverserVector(vector4);
+        assertArrayEquals(vectorExpecIve,TestInver);
+    }
+
+    @Test
     public void minMax()  {
         int vector5[]={1,2,3,4,5} ;
         int vectorExpecMinMax []= {1,5};
@@ -41,5 +58,12 @@ public class VectorHelperTest {
 
     }
 
+    @Test
+    public void formule()  {
+        int vector6[]={1,2,3,4,5} ;
+        int vectorExpecfor []= {1,4,9,16,25};
+        int  TestFor [] = VectorHelper.Formule(vector6);
+        assertArrayEquals(vectorExpecfor,TestFor);
+    }
 
 }
